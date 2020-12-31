0081be41e1450b3206532a1ff5746f6ed2b9b689
Sanford Pun <punsanford@gmail.com>
2020-12-31 15:35:08 +0800

    Auto load an image on webapp start-up

--- webapp/dev/index.js
+++ webapp/dev/index.js
@@ -3,20 +3,22 @@ import { RawScanner } from "symcode";
 const canvas = document.getElementById('frame');
 const ctx = canvas.getContext('2d');
 const img = new Image();
-document.getElementById('imageInput').addEventListener('change', function (e) {
-    const file = this.files[0];
-    img.src = URL.createObjectURL(file);
+document.getElementById('imageInput').addEventListener('change', function (e) { loadImageFromSource(this.files[0]) });
+
+document.addEventListener('load', loadImageFromSource('./assets/sample_code.jpg'));
+
+function loadImageFromSource(source) {
+    console.log("HI");
+    img.src = source instanceof File ? URL.createObjectURL(source) : source;
     img.onload = function () {
         canvas.width = img.naturalWidth;
         canvas.height = img.naturalHeight;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0);
-
-        const frame = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        scanForFinders(frame);
+        scanForFinders();
     };
-});
+}
 
-function scanForFinders(frame) {
+function scanForFinders() {
     console.log(RawScanner.scan_from_canvas_id('frame'));
 }
