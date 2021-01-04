b6da706c44dabbc9ec1ec3e433fe170ae67a363a
Sanford Pun <punsanford@gmail.com>
2021-01-04 13:23:54 +0800

    Add render_color_image_to_canvas utility

--- webapp/src/utils.rs
+++ webapp/src/utils.rs
@@ -1,4 +1,8 @@
-use visioncortex::{ColorHsv, Sampler, Shape};
+use visioncortex::{ColorHsv, ColorImage, Sampler, Shape};
+use wasm_bindgen::{Clamped, JsValue};
+use web_sys::ImageData;
+
+use crate::canvas::Canvas;
 
 extern crate cfg_if;
 
@@ -14,13 +18,14 @@ cfg_if::cfg_if! {
     }
 }
 
-// Check Saturation and Value in HSV
+/// Check Saturation and Value in HSV
 pub(crate) fn is_black(color: &ColorHsv) -> bool {
     const BLACK_LIMIT: f64 = 0.25;
     color.s <= BLACK_LIMIT &&
     color.v <= BLACK_LIMIT
 }
 
+/*
 /// First attempt: When locating Finder patterns, make the shape square before checking if it is a circle to deal with distortion
 pub(crate) fn make_shape_square(original: &Shape) -> Shape {
     if original.image.width == original.image.height {
@@ -31,4 +36,11 @@ pub(crate) fn make_shape_square(original: &Shape) -> Shape {
             image: Sampler::resample_image(&original.image, max_side, max_side)
         }
     }
+}
+*/
+
+pub(crate) fn render_color_image_to_canvas(image: ColorImage, canvas: &Canvas) -> Result<(), JsValue> {
+    let mut data = image.pixels.clone();
+    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut data), image.width as u32, image.height as u32)?;
+    canvas.get_rendering_context_2d().put_image_data(&data, 0.0, 0.0)
 }
