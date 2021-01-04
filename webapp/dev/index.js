522a9381dbe17d7d7850e4422b81b2aeb5291ae3
Sanford Pun <punsanford@gmail.com>
2021-01-04 16:07:10 +0800

    Add debug canvas support in js

--- webapp/dev/index.js
+++ webapp/dev/index.js
@@ -1,18 +1,24 @@
 import { RawScanner } from "symcode";
 
 const canvas = document.getElementById('frame');
+const debugCanvas = document.getElementById('debug');
 const ctx = canvas.getContext('2d');
+const debugCtx = debugCanvas.getContext('2d');
 const img = new Image();
 document.getElementById('imageInput').addEventListener('change', function (e) { loadImageFromSource(this.files[0]) });
 
-document.addEventListener('load', loadImageFromSource('./assets/sample_code_with_noise.jpg'));
+//document.addEventListener('load', loadImageFromSource('./assets/sample_code_with_noise.jpg'));
 
 function loadImageFromSource(source) {
-    console.log("HI");
     img.src = source instanceof File ? URL.createObjectURL(source) : source;
     img.onload = function () {
         canvas.width = img.naturalWidth;
         canvas.height = img.naturalHeight;
+
+        debugCanvas.width = canvas.width;
+        debugCanvas.height = canvas.height;
+        debugCtx.clearRect(0, 0, debugCanvas.width, debugCanvas);
+
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0);
         scanForFinders();
@@ -20,5 +26,5 @@ function loadImageFromSource(source) {
 }
 
 function scanForFinders() {
-    console.log(RawScanner.scan_from_canvas_id('frame', 'debug'));
+    console.log(RawScanner.scan_from_canvas_id('frame', 'debug', 1.5));
 }
