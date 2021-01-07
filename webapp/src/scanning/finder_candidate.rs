e4305b07c1166a5ff9fdf8df44d824f125707afa
Sanford Pun <punsanford@gmail.com>
2021-01-07 10:17:40 +0800

    Move extract_finder_candidates and associated functions from RawScanner to FinderCandidate

--- webapp/src/scanning/finder_candidate.rs
+++ webapp/src/scanning/finder_candidate.rs
@@ -1,6 +1,8 @@
-use visioncortex::{BoundingRect, ColorHsv, Shape, color_clusters::{Cluster, ClustersView}};
+use visioncortex::{BoundingRect, ColorHsv, ColorImage, Shape, color_clusters::{Cluster, ClustersView}};
 
-use super::is_black;
+use crate::{canvas::Canvas, utils::{render_bounding_rect_to_canvas, render_color_image_to_canvas}};
+
+use super::{color_image_to_clusters, is_black};
 
 
 /// Those whose color and shape are close to an actual Finder
@@ -9,6 +11,36 @@ pub(crate) struct FinderCandidate {
     pub(crate) rect: BoundingRect,
 }
 
+/// Operates for/on a collection of FinderCandidate
+impl FinderCandidate {
+    /// Extract the Finder patterns.
+    ///
+    /// Decision is made based on the colors and shapes of each cluster.
+    pub(crate) fn extract_finder_candidates(frame: &ColorImage, canvas: &Canvas, debug_canvas: &Canvas) -> Vec<Self> {
+
+        let clusters = color_image_to_clusters(frame.clone());
+        let view = clusters.view(); // Get the ClustersView (parent of clusters)
+
+        let render_result = render_color_image_to_canvas(&view.to_color_image(), debug_canvas); // Possibly unhandled exception
+        
+        let finder_candidates: Vec<Self> = view.clusters_output.iter()
+            .filter_map(|&cluster_index| Self::from_cluster_and_view(view.get_cluster(cluster_index), &view))
+            .collect();
+        
+        Self::render_finder_candidates(canvas, &finder_candidates);
+
+        finder_candidates
+    }
+
+    fn render_finder_candidates(canvas: &Canvas, finder_candidates: &[Self]) {
+        for finder in finder_candidates.iter() {
+            let rect = &finder.rect;
+            render_bounding_rect_to_canvas(rect, canvas);
+        }
+    }
+}
+
+/// Operates for/on a single FinderCandidate
 impl FinderCandidate {
     pub(crate) fn from_cluster_and_view(cluster: &Cluster, view: &ClustersView) -> Option<Self> {
         let color = cluster.color().to_hsv();
--- webapp/src/scanning/raw_scanner.rs
+++ webapp/src/scanning/raw_scanner.rs
@@ -1,10 +1,9 @@
-use visioncortex::{ColorImage};
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-use crate::{canvas::Canvas, utils::{render_bounding_rect_to_canvas, render_color_image_to_canvas}};
+use crate::{canvas::Canvas, utils::render_color_image_to_canvas};
 
-use super::{FinderCandidate, Recognizer, color_image_to_clusters, transform::Transformer};
+use super::{FinderCandidate, Recognizer, transform::Transformer};
 
 #[wasm_bindgen]
 pub struct RawScanner {}
@@ -17,7 +16,7 @@ impl RawScanner {
         let debug_canvas = &Canvas::new_from_id(debug_canvas_id);
 
         let raw_frame = canvas.get_image_data_as_color_image(0, 0, canvas.width() as u32, canvas.height() as u32);
-        let finder_candidates = Self::extract_finder_candidates(
+        let finder_candidates = FinderCandidate::extract_finder_candidates(
             &raw_frame,
             canvas,
             debug_canvas
@@ -35,34 +34,6 @@ impl RawScanner {
             "Recognition complete".into()
         } else {
             "Cannot rectify image".into()
-        }
-    }
-}
-
-impl RawScanner {
-    /// Extract the Finder patterns.
-    ///
-    /// Decision is made based on the colors and shapes of each candidate.
-    fn extract_finder_candidates(frame: &ColorImage, canvas: &Canvas, debug_canvas: &Canvas) -> Vec<FinderCandidate> {
-
-        let clusters = color_image_to_clusters(frame.clone());
-        let view = clusters.view(); // Get the ClustersView (parent of clusters)
-
-        let render_result = render_color_image_to_canvas(&view.to_color_image(), debug_canvas); // Possibly unhandled exception
-        
-        let finder_candidates: Vec<FinderCandidate> = view.clusters_output.iter()
-            .filter_map(|&cluster_index| FinderCandidate::from_cluster_and_view(view.get_cluster(cluster_index), &view))
-            .collect();
-        
-        Self::render_finder_candidates(canvas, &finder_candidates);
-
-        finder_candidates
-    }
-
-    fn render_finder_candidates(canvas: &Canvas, finder_candidates: &[FinderCandidate]) {
-        for finder in finder_candidates.iter() {
-            let rect = &finder.rect;
-            render_bounding_rect_to_canvas(rect, canvas);
         }
     }
 }
