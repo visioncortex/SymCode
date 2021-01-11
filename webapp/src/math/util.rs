70c96b911fa321f9cc53ae244d97b641e1a14d32
Sanford Pun <punsanford@gmail.com>
2021-01-11 11:31:30 +0800

    Add util to check clockwiseness of 3 points

--- webapp/src/math/util.rs
+++ webapp/src/math/util.rs
@@ -1,12 +1,24 @@
 use visioncortex::PointF64;
 
+pub(crate) const EPSILON: f64 = 1e-5;
+
 pub(crate) fn euclid_dist_f64(p1: &PointF64, p2: &PointF64) -> f64 {
     (*p1-*p2).norm()
 }
 
-/// Returns true iff the traversal p1->p2->p3 is in clockwise order
+/// Returns true iff the traversal p1->p2->p3 is in clockwise order and not collinear.
+///
+/// Assumes origin in top-left corner.
 pub(crate) fn clockwise_points_f64(p1: &PointF64, p2: &PointF64, p3: &PointF64) -> bool {
-    true
+    let cross_product_z_component = |a: PointF64, b: PointF64| { a.x * b.y - a.y * b.x };
+
+    let p1p2 = *p2 - *p1;
+    let p1p3 = *p3 - *p1;
+
+    // (p1p2 x p1p3).z > 0 iff the traversal p1->p2->p3 is in clockwise order
+    let cross_z = cross_product_z_component(p1p2, p1p3);
+
+    cross_z > EPSILON && cross_z.is_sign_positive()
 }
 
 #[cfg(test)]
@@ -15,7 +27,7 @@ mod tests {
     use super::*;
 
     fn f64_approximately(a: f64, b: f64) -> bool {
-        (a - b).abs() <= 1e-5
+        (a - b).abs() <= EPSILON
     }
 
     #[test]
@@ -40,5 +52,21 @@ mod tests {
         let p2 = PointF64::new(1.0, 5.0);
         let dist = euclid_dist_f64(&p1, &p2);
         assert!(f64_approximately(dist, 5.0_f64.sqrt()))
+    }
+
+    #[test]
+    fn test_clockwise_points() {
+        // Clockwise
+        assert!(clockwise_points_f64(&PointF64::new(0.0, 0.0), &PointF64::new(1.0, 0.0), &PointF64::new(0.0, 1.0)));
+        // Anti-Clockwise
+        assert!(!clockwise_points_f64(&PointF64::new(0.0, 0.0), &PointF64::new(0.0, 1.0), &PointF64::new(1.0, 0.0)));
+        // Collinear
+        assert!(!clockwise_points_f64(&PointF64::new(0.0, 0.0), &PointF64::new(1.0, 1.0), &PointF64::new(2.0, 2.0)));
+        // Same point
+        assert!(!clockwise_points_f64(&PointF64::new(1.0, 1.0), &PointF64::new(1.0, 1.0), &PointF64::new(1.0, 1.0)));
+        // Random clockwise
+        assert!(clockwise_points_f64(&PointF64::new(7.3, 5.2), &PointF64::new(10.5, 2.5), &PointF64::new(15.6, 4.2)));
+        // Random anti-clockwise
+        assert!(!clockwise_points_f64(&PointF64::new(23.3, 6.8), &PointF64::new(30.1, 14.7), &PointF64::new(27.5, 11.4)));
     }
 }
