bb68fa797d04373e7d14383cb9a12da645d59894
Sanford Pun <punsanford@gmail.com>
2021-01-12 12:31:08 +0800

    Add util to normalize a PointF64

--- webapp/src/math/util.rs
+++ webapp/src/math/util.rs
@@ -2,6 +2,11 @@ use visioncortex::PointF64;
 
 pub(crate) const EPSILON: f64 = 1e-5;
 
+pub(crate) fn normalize_point_f64(p: &PointF64) -> PointF64 {
+    let norm = p.norm();
+    PointF64::new(p.x / norm, p.y / norm)
+}
+
 pub(crate) fn euclid_dist_f64(p1: &PointF64, p2: &PointF64) -> f64 {
     (*p1-*p2).norm()
 }
