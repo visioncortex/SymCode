2c57e17ea927d6f7b4a59da6b73fce4bda6a30cb
Sanford Pun <punsanford@gmail.com>
2021-01-14 12:06:45 +0800

    Working camera

--- webapp/dev/index.js
+++ webapp/dev/index.js
@@ -6,9 +6,19 @@ const loadBuffer = document.getElementById('loadBuffer');
 const loadBufferCtx = loadBuffer.getContext('2d');
 const debugCanvas = document.getElementById('debug');
 const ctx = canvas.getContext('2d');
-const debugCtx = debugCanvas.getContext('2d');
+const camera = document.getElementById('camera');
+const cameraButton = document.getElementById('cameraButton');
+const cameraInputBuffer = document.getElementById('cameraInputBuffer');
+const cameraInputBufferCtx = cameraInputBuffer.getContext('2d');
 const img = new Image();
 const numTemplates = 4;
+
+const inputFrameSize = {
+    width: 720,
+    height: 720,
+};
+const fps = 30;
+
 document.getElementById('imageInput').addEventListener('change', function (e) { scanImageFromSource(this.files[0]) });
 
 document.addEventListener('load', loadAlphabet());
@@ -86,4 +96,57 @@ function loadAlphabet() {
 function loadingCompletes() {
     console.log("Template loading completes.");
     scanImageFromSource("assets/camera_inputs/test_prototype_3/0.jpg");
+}
+
+const constraints = {
+    video: { width: {min: 720}, height: {min: 720} },
+};
+
+cameraButton.onclick = function() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(handleSuccess)
+        .catch(handleError);
+}
+
+function handleSuccess(stream) {
+    camera.style.display = "block";
+    camera.srcObject = stream;
+    getCameraVideoDimensions()
+        .then(({width, height}) => {
+            startStreaming(width, height);
+        });
+}
+
+function handleError(error) {
+    console.error("Error: ", error);
+}
+
+function getCameraVideoDimensions() {
+    return new Promise(function(resolve) {
+        camera.addEventListener("loadedmetadata", function () {
+            let width = this.videoWidth;
+            let height = this.videoHeight;
+            resolve({
+                width: width,
+                height: height,
+            });
+        }, false);
+    });
+}
+
+function startStreaming(videoWidth, videoHeight) {
+    console.log("Start streaming");
+    console.log(videoWidth + " " + videoHeight);
+    cameraInputBuffer.width = inputFrameSize.width;
+    cameraInputBuffer.height = inputFrameSize.height;
+    const dx = (videoWidth - inputFrameSize.width) / 2;
+    const dy = (videoHeight - inputFrameSize.height) / 2;
+
+    drawFrame(dx, dy);
+}
+
+function drawFrame(dx, dy) {
+    cameraInputBufferCtx.clearRect(0, 0, cameraInputBuffer.width, cameraInputBuffer.height);
+    cameraInputBufferCtx.drawImage(camera, dx, dy, inputFrameSize.width, inputFrameSize.height);
 }
