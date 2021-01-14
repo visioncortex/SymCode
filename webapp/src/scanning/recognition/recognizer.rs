2613536392f847fcd9f37bfe785009847421c192
Sanford Pun <punsanford@gmail.com>
2021-01-14 14:46:01 +0800

    Tune merge expand to 10 units xy

--- webapp/src/scanning/recognition/recognizer.rs
+++ webapp/src/scanning/recognition/recognizer.rs
@@ -9,7 +9,7 @@ pub struct Recognizer {}
 
 impl Recognizer {
     pub fn recognize_glyphs_on_image(image: ColorImage, anchor_error_threshold: f64, glyph_library: &GlyphLibrary, stat_tolerance: f64, debug_canvas: &Option<Canvas>) -> GlyphCode {
-        let images_rects = color_image_to_merged_clusters(image, 7, 7); // Expand each cluster by 7 units vertically and horizontally
+        let images_rects = color_image_to_merged_clusters(image, 10, 10); // Expand each cluster by 10 units vertically and horizontally
         
         let mut glyph_code = GlyphCode::default();
         glyph_code.add_images_rects_near_anchors(images_rects, anchor_error_threshold, glyph_library, stat_tolerance, debug_canvas);
