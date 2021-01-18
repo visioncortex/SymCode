fb142782861e126024544094337aae3c3a05007c
Sanford Pun <punsanford@gmail.com>
2021-01-18 17:52:18 +0800

    Write GlyphReader trait

--- webapp/src/scanning/reader.rs
+++ webapp/src/scanning/reader.rs
@@ -12,8 +12,6 @@ pub trait GlyphReader {
 
     type Library;
 
-    type Params;
-
     fn crop_at_anchor(anchor: &PointF64, image: &BinaryImage, symcode_config: &SymcodeConfig, absolute_empty_cluster_threshold: u64) -> Option<BinaryImage> {
         let rect = BoundingRect::new_x_y_w_h(anchor.x as i32, anchor.y as i32, symcode_config.symbol_width as i32, symcode_config.symbol_height as i32);
         if let Some(canvas) = symcode_config.debug_canvas {
@@ -28,7 +26,7 @@ pub trait GlyphReader {
     }
 
     /// Finds the most similar glyph in the library based on given params
-    fn find_most_similar_glyph(glyph_library: &Self::Library, image: BinaryImage, params: Self::Params) -> Self::Label;
+    fn find_most_similar_glyph(image: BinaryImage, glyph_library: &Self::Library, symcode_config: &crate::scanning::SymcodeConfig) -> Self::Label;
 
     /// Read all glyphs at the anchors on the input image
     fn read_glyphs_from_rectified_image(image: visioncortex::BinaryImage, glyph_library: &Self::Library, symcode_config: &crate::scanning::SymcodeConfig) -> Vec<Option<Self::Label>> {
@@ -36,7 +34,7 @@ pub trait GlyphReader {
         symcode_config.glyph_anchors.iter()
             .map(|anchor| {
                 let crop = Self::crop_at_anchor(anchor, &image, symcode_config, absolute_empty_cluster_threshold)?;
-                Some(Self::find_most_similar_glyph(crop, symcode_config.stat_tolerance, symcode_config.max_encoding_difference))
+                Some(Self::find_most_similar_glyph(crop, glyph_library, symcode_config))
             })
             .collect()
     }
