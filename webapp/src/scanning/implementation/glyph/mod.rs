6c73c5893c2e6a7abc7fdaa610d915119173f34e
Sanford Pun <punsanford@gmail.com>
2021-01-20 15:00:20 +0800

    glyph.rs -> glyph_element.rs

--- webapp/src/scanning/implementation/glyph/mod.rs
+++ webapp/src/scanning/implementation/glyph/mod.rs
@@ -1,9 +1,9 @@
-pub mod glyph;
+pub mod glyph_element;
 pub mod trace;
 pub mod stats;
 pub mod library;
 
-pub use glyph::*;
+pub use glyph_element::*;
 pub use trace::*;
 pub use stats::*;
 pub use library::*;
