c18c64ebc21a2347f8f9e6382d59daa722a8b787
Sanford Pun <punsanford@gmail.com>
2021-01-20 15:04:55 +0800

    Refactor GlyphReader

--- webapp/src/scanning/reader.rs
+++ webapp/src/scanning/reader.rs
@@ -5,17 +5,19 @@ use visioncortex::{BinaryImage, BoundingRect, PointF64};
 use super::{SymcodeConfig, render_bounding_rect_to_canvas};
 
 pub trait GlyphReader {
-    // Input = BinaryImage
-    // Output = Option<Vec<Glyph>>
+    // Input = BinaryImage, Library
+    // Output = Option<Vec<Label>>
 
     type Label;
 
     type Library;
 
+    /// Assuming the perspective transform done in the previous stage is accurate,
+    /// the areas at the anchors should be where the glyphs are.
     fn crop_at_anchor(anchor: &PointF64, image: &BinaryImage, symcode_config: &SymcodeConfig, absolute_empty_cluster_threshold: u64) -> Option<BinaryImage> {
         let rect = BoundingRect::new_x_y_w_h(anchor.x as i32, anchor.y as i32, symcode_config.symbol_width as i32, symcode_config.symbol_height as i32);
-        if let Some(canvas) = &symcode_config.debug_canvas {
-            render_bounding_rect_to_canvas(&rect, canvas);
+        if let Some(debug_canvas) = &symcode_config.debug_canvas {
+            render_bounding_rect_to_canvas(&rect, debug_canvas);
         }
         let cluster = image.crop_with_rect(rect);
         if cluster.area() <= absolute_empty_cluster_threshold {
@@ -29,7 +31,7 @@ pub trait GlyphReader {
     fn find_most_similar_glyph(image: BinaryImage, glyph_library: &Self::Library, symcode_config: &crate::scanning::SymcodeConfig) -> Self::Label;
 
     /// Read all glyphs at the anchors on the input image
-    fn read_glyphs_from_rectified_image(image: visioncortex::BinaryImage, glyph_library: &Self::Library, symcode_config: &crate::scanning::SymcodeConfig) -> Vec<Option<Self::Label>> {
+    fn read_glyphs_from_rectified_image(image: BinaryImage, glyph_library: &Self::Library, symcode_config: &crate::scanning::SymcodeConfig) -> Vec<Option<Self::Label>> {
         let absolute_empty_cluster_threshold = (symcode_config.empty_cluster_threshold * (symcode_config.symbol_width * symcode_config.symbol_height) as f64) as u64;
         symcode_config.glyph_anchors.iter()
             .map(|anchor| {
