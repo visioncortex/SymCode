f3f93ee23b479a4687be14d670b869a5e024f680
Sanford Pun <punsanford@gmail.com>
2021-01-22 17:12:40 +0800

    Add button to export raw frame

--- webapp/dev/index.js
+++ webapp/dev/index.js
@@ -102,6 +102,35 @@ function scan() {
     });
 }
 
+if (document.getElementById('export')) {
+    document.getElementById('export').addEventListener('click', function (e) {
+        let filename = 'visionmagic-' + new Date().toISOString().slice(0, 19).replace(/:/g, '').replace('T', ' ') + '.png';
+
+        /// create an "off-screen" anchor tag
+        let lnk = document.createElement('a');
+
+        /// the key here is to set the download attribute of the a tag
+        lnk.download = filename;
+
+        /// convert canvas content to data-uri for link. When download
+        /// attribute is set the content pointed to by link will be
+        /// pushed as "download" in HTML5 capable browsers
+        lnk.href = frameCanvas.toDataURL("image/png;base64");
+
+        /// create a "fake" click-event to trigger the download
+        if (document.createEvent) {
+            e = document.createEvent("MouseEvents");
+            e.initMouseEvent("click", true, true, window,
+                0, 0, 0, 0, 0, false, false, false,
+                false, 0, null);
+
+            lnk.dispatchEvent(e);
+        } else if (lnk.fireEvent) {
+            lnk.fireEvent("onclick");
+        }
+    }, false);
+}
+
 const constraints = {
     video: { width: {min: 720}, height: {min: 720} },
 };
