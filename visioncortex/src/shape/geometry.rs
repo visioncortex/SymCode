633aa603d734b60784fed22f3ed87a0d305714b8
Chris Tsang <tyt2y7@gmail.com>
2021-02-22 02:17:38 +0800

    Shape is quadrilateral

--- visioncortex/src/shape/geometry.rs
+++ visioncortex/src/shape/geometry.rs
@@ -1,4 +1,5 @@
-use crate::{BinaryImage, BoundingRect, clusters::Cluster, PathSimplifyMode, PointI32};
+use crate::{BinaryImage, BoundingRect, clusters::Cluster, CompoundPathElement, PathSimplifyMode, PointI32};
+use super::rasterizer::rasterize_triangle;
 
 /// A conceptual object represented by an image
 #[derive(Clone)]
@@ -130,6 +131,10 @@ impl Shape {
         let threshold = std::cmp::min(self.image.width, self.image.height);
         let threshold = (threshold * threshold) / 4;
         let diff = self.image.diff(&Self::ellipse(self.image.width, self.image.height).image);
+        Self::clustered_diff(&diff, threshold)
+    }
+
+    fn clustered_diff(diff: &BinaryImage, threshold: usize) -> bool {
         let clusters = diff.to_clusters(false);
         let mut sum = 0;
         for cluster in clusters.iter() {
@@ -143,7 +148,7 @@ impl Shape {
     }
 
     pub fn is_quadrilateral(&self) -> bool {
-        let mut path = Cluster::image_to_compound_path(
+        let mut paths = Cluster::image_to_compound_path(
             &PointI32::default(),
             &self.image,
             PathSimplifyMode::None,
@@ -152,10 +157,22 @@ impl Shape {
             0,
             0.0
         );
-        path.paths.truncate(1);
-        let path = path.reduce((self.image.width + self.image.height) as f64);
+        paths.paths.truncate(1);
+        let paths = paths.reduce(std::cmp::min(self.image.width, self.image.height) as f64);
         // the path is reduced to a quadrilateral bound by the north most, east most, south most and west most point
-        true
+        let mut reduced = BinaryImage::new_w_h(self.image.width, self.image.height);
+        let path = &match &paths.paths[0] {
+            CompoundPathElement::PathI32(path) => path,
+            _ => unreachable!(),
+        }.path;
+        let p0 = PointI32::new(path[0].x-1, path[0].y);
+        let p2 = PointI32::new(path[2].x, path[2].y-1);
+        rasterize_triangle(&[p0, PointI32::new(path[1].x-1, path[1].y-1), p2], &mut reduced);
+        rasterize_triangle(&[p0, p2, PointI32::new(path[3].x, path[1].y-1)], &mut reduced);
+        // panic!("\n{}", reduced.to_string());
+        let diff = self.image.diff(&reduced);
+        let threshold = self.image.width * self.image.height / 6;
+        Self::clustered_diff(&diff, threshold)
     }
 }
 
@@ -280,5 +297,52 @@ mod tests {
             "*******\n" +
             "--***--\n"
         );
+    }
+
+    #[test]
+    fn is_quadrilateral_test_1() {
+        assert!(!Shape::from(BinaryImage::from_string(&(
+            "--***--\n".to_owned() +
+            "-*****-\n" +
+            "*******\n" +
+            "*******\n" +
+            "*******\n" +
+            "-*****-\n" +
+            "--***--\n"
+        ))).is_quadrilateral());
+    }
+
+    #[test]
+    fn is_quadrilateral_test_2() {
+        assert!(Shape::from(BinaryImage::from_string(&(
+            "----*----\n".to_owned() +
+            "---***---\n" +
+            "--*****--\n" +
+            "-*******-\n" +
+            "*********\n" +
+            "*********\n" +
+            "*********\n" +
+            "-*******-\n" +
+            "--*****--\n" +
+            "---***---\n" +
+            "----*----\n"
+        ))).is_quadrilateral());
+    }
+
+    #[test]
+    fn is_quadrilateral_test_3() {
+        assert!(!Shape::from(BinaryImage::from_string(&(
+            "----*----\n".to_owned() +
+            "--*****--\n" +
+            "-*******-\n" +
+            "-*******-\n" +
+            "*********\n" +
+            "*********\n" +
+            "*********\n" +
+            "-*******-\n" +
+            "-*******-\n" +
+            "--*****--\n" +
+            "----*----\n"
+        ))).is_quadrilateral());
     }
 }
--- visioncortex/src/shape/mod.rs
+++ visioncortex/src/shape/mod.rs
@@ -1,9 +1,11 @@
 mod geometry;
 mod image_operations;
 mod processor;
+mod rasterizer;
 mod skeleton;
 
 pub use geometry::*;
 pub use image_operations::*;
 pub use processor::*;
+// pub use rasterizer::*;
 pub use skeleton::*;
