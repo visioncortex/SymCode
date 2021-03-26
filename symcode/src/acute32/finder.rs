86ca8511e461a9a2dfc93892b5e43d885f8785b3
Sanford Pun <punsanford@gmail.com>
2021-03-26 15:10:03 +0800

    Local adaptive thresholding

--- symcode/src/acute32/finder.rs
+++ symcode/src/acute32/finder.rs
@@ -85,7 +85,7 @@ impl<'a> FinderInterface for Acute32FinderCandidate<'a> {
         // Get the reference to the input raw frame
         let raw_frame = input;
         // Binarize
-        let binary_raw_frame = binarize_image_util(raw_frame);
+        let binary_raw_frame = binarize_image_util(raw_frame, 40, 50);
         config.debugger.render_binary_image_to_canvas(&binary_raw_frame)?;
 
         // Processing starts
--- symcode/src/acute32/util.rs
+++ symcode/src/acute32/util.rs
@@ -1,27 +1,25 @@
-use visioncortex::{BinaryImage, Color, ColorImage, PointF64, PointI32, SampleStatBuilder};
+use visioncortex::{BinaryImage, Color, ColorImage, PointF64, PointI32, SampleStatBuilder, SummedAreaTable};
 
-pub(crate) fn binarize_image_util(color_image: &ColorImage) -> BinaryImage {
-    let threshold = threshold_for(color_image);
-    color_image.to_binary_image(move |c| {
-        let r = c.r as u32;
-        let g = c.g as u32;
-        let b = c.b as u32;
-    
-        r + g + b < 3*threshold
-    })
-}
+// Local adaptive thresholding by finding patch mean around each pixel
+pub(crate) fn binarize_image_util(color_image: &ColorImage, patch_size: usize, offset: i32) -> BinaryImage {
+    let sat = SummedAreaTable::from_color_image(color_image);
 
-fn threshold_for(image: &ColorImage) -> u32 {
-    let mut stat = SampleStatBuilder::new();
-    for y in 0..image.height {
-        for x in 0..image.width {
-            let c = image.get_pixel(x, y);
-            let c_sum = (c.r as u32 + c.g as u32 + c.b as u32) as i32;
-            stat.add(c_sum);
+    let mut result = BinaryImage::new_w_h(color_image.width, color_image.height);
+
+    let half_patch_size = patch_size >> 1;
+    for y in 0..result.height {
+        for x in 0..result.width {
+            let top_left = PointI32::new(std::cmp::max(0, (x - half_patch_size) as i32), std::cmp::max(0, (y - half_patch_size) as i32));
+            let bot_right = PointI32::new(std::cmp::min(result.width as i32 - 1, (x + half_patch_size) as i32), std::cmp::min(result.height as i32 - 1, (y + half_patch_size) as i32));
+            let threshold = std::cmp::max(0, sat.get_region_mean_top_left_bot_right(top_left, bot_right) as i32 - offset) as u8;
+
+            let c = color_image.get_pixel(x, y);
+            let c_mean = ((c.r as u32 + c.g as u32 + c.b as u32) / 3) as u8;
+            result.set_pixel(x, y, c_mean < threshold);
         }
     }
-    stat.build();
-    (stat.percentile(10) + stat.percentile(90)) as u32 / 6
+
+    result
 }
 
 pub(crate) fn is_black_rgb(color: &Color) -> bool {
